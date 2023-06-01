1d02e026c770cbfca1adfc6feec5effb63442383
vtanh1905@gmail.com <vtanh1905@gmail.com>
2023-06-01 09:56:50 +0700

    Setup Prettierrc and Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  // Setup Swagger UI
+  const swaggerDocumentOptions = new DocumentBuilder()
+    .setTitle('Book Store')
+    .setDescription('The book store API description')
+    .setVersion('1.0')
+    .build()
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerDocumentOptions)
+  SwaggerModule.setup('api-docs', app, swaggerDocument)
+
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
